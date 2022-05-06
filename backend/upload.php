4d5e03dc8100b33af8f573ddbee0767081f155aa
ykononov1984 <ykononov@hill30.com>
2022-05-06 18:40:23 +0300

    added multiple upload ability to frontend and backend

--- backend/upload.php
+++ backend/upload.php
@@ -1,27 +1,44 @@
 <?php
 error_reporting(0);
 header("Access-Control-Allow-Origin: *");
-//header('Content-type: application/json; charset=utf-8');
+header('Content-type: application/json; charset=utf-8');
 //echo("<pre>");
 //print_r($_FILES);
 //echo("</pre>");
+//echo(count($_FILES));
 //goto end;
 try {
     //break;
-    if (!isset($_FILES['sample_image'])) {
+    if (count($_FILES) === 0) {
         throw new Exception("file don't received to server!");
     }
 
-    $extension = pathinfo($_FILES["sample_image"]["name"], PATHINFO_EXTENSION);
-    $new_name = time().'.'.$extension;
-
-    if (!move_uploaded_file($_FILES['sample_image']['tmp_name'], 'images/' . $new_name)) {
-        throw new Exception("Could not move file; bad credentials of target folder!");
+    $output_array = [];
+    foreach ($_FILES as $key => $value) {
+        $extension = pathinfo($value["name"], PATHINFO_EXTENSION);
+        $new_name = time().'_'.$key.'.'.$extension;
+        if (!move_uploaded_file($value['tmp_name'], 'images/' . $new_name)) {
+            throw new Exception("Could not move file; bad credentials of target folder!");
+            break;
+        }
+        array_push($output_array, 
+        [
+            'image_source' => 'images/' . $new_name, 
+            'file_size' => $value["size"],
+            'old_name' => $value["name"]
+        ]);
     }
-    $data = array(
-        'image_source' => 'images/' . $new_name
+
+    //echo("<pre>");
+        //print_r($output_array);
+        //echo("</pre>");
+    sleep(5);
+    $output_array = array(
+        "images_array" => $output_array
     );
-    echo json_encode($data);
+
+    echo json_encode($output_array, JSON_PRETTY_PRINT);  
+
 } catch (Exception $e) {
     $data_error = array(
         'error_name' => $e->getMessage()
